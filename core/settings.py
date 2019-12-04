1f60cabadf76a8ae5e768107c89e5b886bf32ddb
Frederick Johnson <fritzj96@gmail.com>
2019-12-04 12:14:37 -0800

    added: dj_database_url as the default db connection scheme

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from django.utils.timezone import timedelta
+import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -78,11 +79,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
 }
+DATABASES['default'] = dj_database_url.config(
+    default='postgres://localhost:5432/account-app')
 
 
 # Password validation
