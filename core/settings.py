59d0e0b1831e85058fca619d1e607c3c41f1fd3b
Frederick Johnson <fritzj96@gmail.com>
2019-12-04 12:08:42 -0800

    added: django-heroku config

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from django.utils.timezone import timedelta
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,3 +134,5 @@ SIMPLE_JWT = {
 
 # Not for "real" production apps
 CORS_ORIGIN_ALLOW_ALL = True
+
+django_heroku.settings(locals())
