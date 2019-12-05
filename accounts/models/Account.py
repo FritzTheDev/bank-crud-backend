4227d77216ecb7b9a40ba9bee8831c04ea5325ca
Frederick Johnson <fritzj96@gmail.com>
2019-12-04 16:02:29 -0800

    fix: added account_type_choices tuple to choices kwarg

--- accounts/models/Account.py
+++ accounts/models/Account.py
@@ -27,6 +27,6 @@ class Account(Model):
         (SAVINGS, 'Savings'),
         (MONEY_MARKET, 'Money Market'),
     )
-    type = CharField(max_length=3)
+    type = CharField(max_length=3, choices=account_type_choices)
     # Make sure to document that this is in cents, not (dollars/euros/etc)
     balance = IntegerField(default=0)
