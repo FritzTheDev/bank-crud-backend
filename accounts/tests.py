2139b2bfdc082abc28d0fa5daee2b8cf82e0fa43
Frederick Johnson <fritzj96@gmail.com>
2019-12-04 16:31:03 -0800

    fix: mocking the user foreignkey properly

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,13 +1,16 @@
 from django.test import TestCase
 from accounts.models.Account import Account
+from django.contrib.auth.models import User
 
 
 class AccountModelTests(TestCase):
     test_account_name = "Vacation Fund"
 
     def setUp(self):
+        example_user = User.objects.create_user(
+            "Freddie Firestarter", email="Arson@Crime.com")
         Account.objects.create(
-            owner=1, type=Account.SAVINGS, name=self.test_account_name)
+            owner=example_user, type=Account.SAVINGS, name=self.test_account_name)
 
     def test_string_representation(self):
         test_account = Account.objects.get(name=self.test_account_name)
