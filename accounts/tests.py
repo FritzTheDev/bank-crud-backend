66fd241742f85bd6681188a4eb4661598098a3af
Frederick Johnson <fritzj96@gmail.com>
2019-12-04 16:21:04 -0800

    added: unit test to check if the selfstring method is present & works

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from accounts.models.Account import Account
 
-# Create your tests here.
+
+class AccountModelTests(TestCase):
+    test_account_name = "Vacation Fund"
+
+    def setUp(self):
+        Account.objects.create(
+            owner=1, type=Account.SAVINGS, name=self.test_account_name)
+
+    def test_string_representation(self):
+        test_account = Account.objects.get(name=self.test_account_name)
+        self.assertEqual(str(test_account), self.test_account_name)
