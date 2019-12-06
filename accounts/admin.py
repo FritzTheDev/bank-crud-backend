32e2f32a7a4fda35892e44eee04de96543fd7201
Frederick Johnson <fritzj96@gmail.com>
2019-12-06 13:55:26 -0800

    added: registered the Account model in the admin with a decorator

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,14 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, TabularInline, register
 from accounts.models.Account import Account
 from accounts.models.Transaction import Transaction
 
-# Register your models here.
-admin.site.register(Account)
-admin.site.register(Transaction)
+
+class TransactionInline(TabularInline):
+    model = Transaction
+
+
+@register(Account)
+class AccountAdmin(ModelAdmin):
+    inlines = [
+        TransactionInline
+    ]
