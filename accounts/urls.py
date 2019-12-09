b1d2d57a19234d48c073d223a1a991d1217f475c
Frederick Johnson <fritzj96@gmail.com>
2019-12-09 10:23:57 -0800

    added: account views to router

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from accounts.views.User import UserViewSet
+from accounts.views.Account import AccountViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
