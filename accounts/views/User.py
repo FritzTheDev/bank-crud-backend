86cf262969dfdafff5a9e6b0b28b02054c5009c1
Frederick Johnson <fritzj96@gmail.com>
2019-12-16 11:34:39 -0800

    changed: User viewset to only show one user at a time

--- accounts/views/User.py
+++ accounts/views/User.py
@@ -1,11 +1,12 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import RetrieveModelMixin
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from django.contrib.auth.models import User
 from accounts.serializers.User import UserSerializer
 from accounts.permissions import IsUser
 
 
-class UserViewSet(ReadOnlyModelViewSet):
+class UserViewSet(GenericViewSet, RetrieveModelMixin):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsUser]
     queryset = User.objects.all()
