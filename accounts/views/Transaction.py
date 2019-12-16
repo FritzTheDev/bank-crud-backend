4d2f105439662cd94aae7f37c376d649ba43711c
Frederick Johnson <fritzj96@gmail.com>
2019-12-16 11:06:54 -0800

    added: "signed in" auth to Transaction viewset

--- accounts/views/Transaction.py
+++ accounts/views/Transaction.py
@@ -1,8 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 from accounts.models.Transaction import Transaction
 from accounts.serializers.Transaction import TransactionSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class TransactionViewSet(ModelViewSet):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
