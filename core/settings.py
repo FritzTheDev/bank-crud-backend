6978cd362eb9bc1f6d2897b9f7afa0bd7718d71d
Frederick Johnson <fritzj96@gmail.com>
2019-12-16 15:34:59 -0800

    fixed: misplaced )

--- core/settings.py
+++ core/settings.py
@@ -127,4 +127,4 @@ django_heroku.settings(locals())
 
 
 DATABASES['default'] = dj_database_url.parse(os.environ.get(
-    'DATABASE_URL', 'postgres://localhost:5432/account-app'), conn_max_age=600, ssl_require=True)
+    'DATABASE_URL', 'postgres://localhost:5432/account-app'), conn_max_age=600)
