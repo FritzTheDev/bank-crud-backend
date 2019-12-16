39c02017366e4979bc10340e097a26f8f15fdc4e
Frederick Johnson <fritzj96@gmail.com>
2019-12-16 11:31:54 -0800

    changed: AccountViewSet to be read-one only

--- accounts/views/Account.py
+++ accounts/views/Account.py
@@ -1,12 +1,13 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import RetrieveModelMixin
 from accounts.models.Account import Account
 from accounts.serializers.Account import AccountSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from accounts.permissions import IsTransactionOwner
+from accounts.permissions import IsAccountOwner
 
 
-class AccountViewSet(ModelViewSet):
+class AccountViewSet(GenericViewSet, RetrieveModelMixin):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsTransactionOwner]
+    permission_classes = [IsAccountOwner]
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
