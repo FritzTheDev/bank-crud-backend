5173840c18ee063ad8e825c244c5471c2f83f05d
Frederick Johnson <fritzj96@gmail.com>
2019-12-16 11:53:07 -0800

    changed:  TransactionViewSet to use Mixins

--- accounts/views/Transaction.py
+++ accounts/views/Transaction.py
@@ -1,11 +1,12 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import RetrieveModelMixin
 from accounts.models.Transaction import Transaction
 from accounts.serializers.Transaction import TransactionSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from accounts.permissions import IsTransactionOwner
 
 
-class TransactionViewSet(ModelViewSet):
+class TransactionViewSet(GenericViewSet, RetrieveModelMixin):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsTransactionOwner]
     queryset = Transaction.objects.all()
